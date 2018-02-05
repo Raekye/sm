11d608f97d9b035fbed27a4b1ae0db8d676302ed
Carl Lam <lamcarl@Carls-MacBook-Pro.local>
2018-02-05 01:03:09 -0500

    level up

--- cqt.py
+++ cqt.py
@@ -7,13 +7,14 @@ import sm
 import numpy as np
 
 BINS_PER_NOTE = 4
+HOP_LENGTH = 512
 
 def doCQT(data, sr, inDB=False):
-    cqt = librosa.cqt(data, sr=sr, fmin=librosa.note_to_hz('A0'), n_bins=88*BINS_PER_NOTE, bins_per_octave=12*BINS_PER_NOTE)
+    cqt = librosa.cqt(data, sr=sr, fmin=librosa.note_to_hz('A0'), n_bins=88*BINS_PER_NOTE, bins_per_octave=12*BINS_PER_NOTE, hop_length=HOP_LENGTH)
     if inDB:
         cqt_db = librosa.amplitude_to_db(cqt, ref=np.max)
         return cqt_db
-    return cqt
+    return np.absolute(cqt)
 
 if __name__ == "__main__":
     file_name = sys.argv[1]
@@ -24,7 +25,9 @@ if __name__ == "__main__":
     print y.shape
 
     print("Calculating Constant-Q transform") 
-    CQT_db = doCQT(y, sr)
+    CQT_db = doCQT(y, sr, inDB=True)
+    print(np.amax(CQT_db))
+    print(np.amin(CQT_db))
     print CQT_db.shape
     print("Plotting")
     librosa.display.specshow(CQT_db, sr=sr,x_axis='time', y_axis='cqt_hz', bins_per_octave=12*BINS_PER_NOTE)
